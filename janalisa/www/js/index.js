9df78cb678a79923f48edd6f3e451d401b2b192e
Miguel Eduardo da Silva <miguel@jmsoft.local>
2017-05-02 11:00:13 -0300

    first commit

---
+++ janalisa/www/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("c7ccf0b6-f673-433e-80f3-5bffb84a245d")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+}, false);
